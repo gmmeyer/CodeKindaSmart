4fd7e5fc9c7ee37eff029a50f7b6408a9866433a
App Academy Student <student@appacademy.io>
2014-04-22 17:44:18 -0400

    updates seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user_names = ['']
-user_emails =
+user_names = Array.new(3,'asdfasdf')
+user_emails = Array.new(3,'test@test.test')
+user_passwords = Array.new(3,'password')
+
+
+docs = Document.new
+
+titles = Array.new(3,'asdf')
+bodies = Array.new(3, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.')
+
+3.times do |i|
+
+  Document.create(title: i.to_s + titles[i], document: i.to_s + bodies[i])
+
+end
+
+3.times do |i|
+
+  User.create(username: user_names[i] + i.to_s, email: i.to_s + user_emails[i],
+    password: user_passwords[i], password_confirmation: user_passwords[i])
+
+end
