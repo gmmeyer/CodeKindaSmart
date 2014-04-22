cd26449c4e65e3781e0142ad2e22fb1171406342
App Academy Student <student@appacademy.io>
2014-04-22 14:26:26 -0400

    Fixes typo in database.

--- db/migrate/20140422150703_create_users.rb
+++ db/migrate/20140422150703_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.text :username, null: false
-      t.text :email, null: false
-      t.text :password_digest, null: false
-      t.text :session_token
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token
 
       t.timestamps
     end
