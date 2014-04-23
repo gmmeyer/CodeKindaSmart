0d6c5720d8bf670a549bbaa6bc100539f5f5e8a5
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-04-23 12:39:28 -0400

    beautifies annotation and user models

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,9 +1,9 @@
 class Annotation < ActiveRecord::Base
 
-	validates :title, :body, :user_id, :document_id, 
-		:start_location, :end_location, presence: true
+  validates :title, :body, :user_id, :document_id,
+    :start_location, :end_location, presence: true
 
-	belongs_to :document
-	belongs_to :user
+  belongs_to :document
+  belongs_to :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,36 @@
 class User < ActiveRecord::Base
-	has_secure_password
+  has_secure_password
 
-	validates :password, length: { minimum: 6 }, allow_nil: true
-	validates :username, :email, uniqueness: true
-	validates :username, :session_token, presence: true
-	before_validation :ensure_session_token
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :username, :email, uniqueness: true
+  validates :username, :session_token, presence: true
+  before_validation :ensure_session_token
 
-	has_many :documents, dependent: :destroy
-	has_many :annotations, dependent: :destroy
+  has_many :documents, dependent: :destroy
+  has_many :annotations, dependent: :destroy
 
-	def self.generate_session_token
-		SecureRandom.urlsafe_base64(32)
-	end
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(32)
+  end
 
-	def self.find_by_credentials(username, password)
-		@user = user.find_by(username: username)
+  def self.find_by_credentials(username, password)
+    @user = user.find_by(username: username)
 
-		return @user.authenticate(password)
-	end
+    return @user.authenticate(password)
+  end
 
-	def reset_session_token!
-		self.session_token = self.class.generate_session_token
-		self.save!
-	end
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+  end
 
-	def is_password?(password)
-		self.authenticate(password)
-	end
+  def is_password?(password)
+    self.authenticate(password)
+  end
 
-	private
-	def ensure_session_token
-		self.session_token ||= self.class.generate_session_token
-	end
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 
 end
