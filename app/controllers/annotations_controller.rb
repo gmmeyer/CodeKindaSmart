95137c6caadf2506b3aeab57d095f25fb1c7b964
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-04-23 15:20:16 -0400

    annotation views and controller, document views and partial

---
+++ app/controllers/annotations_controller.rb
@@ -0,0 +1,36 @@
+class AnnotationsController < ApplicationController
+	def index
+	end
+
+	def show
+		@annotation = Annotation.find(params[:id])
+	end
+
+	def new
+		@annotation = Annotation.new
+	end
+
+	def create
+		@annotation = Annotation.new(annotation_params)
+		if @annotation.save
+			redirect_to annotation_url(params[:id])
+		else
+			flash[:errors] = @annotation.errors.full_messages
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def destroy
+		@annotation.find(params[:id])
+		@annotation.destroy
+	end
+
+	private
+	def annotation_params
+		params.require(:annotation).params(:title, :body, :user_id, :document_id, 
+			:start_location, :end_location)
+	end
+end
--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -3,49 +3,61 @@ module DocumentsHelper
 	def annotate_document(id)
 
 		@document = Document.find(id)
-		@annotations = Document.annotations.order("start_location DESC")
+		@annotations = @document.annotations.order("start_location ASC")
 
 		return @document.body if @annotations.empty?
 
 		annotated_body = ""
 
-		annotated_body = @document.body[0, @annotations.first.start_location - 1] if @annotations.first.start_location >= 1
-
-		@annotations.each_with_index do |ann, i|
-
-			annotation_loop(ann, index)
-
+		if @annotations.first.start_location >= 1
+			annotated_body = @document.body[0, @annotations.first.start_location] 
 		end
+
+		annotated_body = annotation_loop(annotated_body)
 
 		annotated_body
 	end
 
-	def annotation_loop(ann, index)
+	def annotation_loop(annotated_body)
 
-		before = @document.body[0, ann.start_location - 1]
-		annotated = @document.body[ann.start_location, ann.end_location]
+		@annotations.each_with_index do |ann, index|
+			before = @document.body[0, ann.start_location - 1]
+			annotated = @document.body[ann.start_location, ann.end_location]
 
-		annotated_body += annotation_link_start(ann) + annotated + "</a>"
+			annotated_body += annotation_link_start(ann) + annotated + "</a>"
 
 		if ann == @annotations.last && ann.end_location < @document.body.length - 1
-			annotated_body += @document.body[@annotations.last.end_location, -1]				
-		elsif ann != @annotations.last && ann.end_location < @annotations[i+1].start_location
-			if ann.end_location - @annotations[i+1].start-location > 1
-				annotated_body += @document.body[ann.end_location + 1, @annotations[i + 1].start_location - 1]
-			else
-				annotated_body += @document.body[ann.end_location + 1]
+
+				annotated_body += @document.body[@annotations.last.end_location, 
+					@document.body.length]
+
+			elsif ann != @annotations.last && ann.end_location < @annotations[index + 1].start_location
+				if @annotations[index + 1].start_location - ann.end_location > 1
+
+					annotated_body += @document.body[ann.end_location + 1, 
+						@annotations[index + 1].start_location]
+
+				else
+					annotated_body += @document.body[ann.end_location + 1]
+				end
 			end
 		end
 
+		return annotated_body
 	end
 
 	def annotation_link_start(annotation)
 
 		a_class = "annotation"
-		a_class += " my_annotation" if annotation.user_id == current_user.id
+		a_class += " my_annotation" if current_user && current_user.id == annotation.user_id
+		a_url = annotation_url(annotation.id)
 
-		return URI.escape(<<-HTML)
-			<a class="<%=a_link%>" href="<%=annotation(annotation.id)%>">
-		HTML
+		anchor_tag = "<a class='annotation-link' href=" + annotation_url(annotation.id) + ">"
+
+		# anchor_tag = URI.escape(<<-HTML)
+		# 	<a class="annotation-link" href=annotation_url(annotation.id)>
+		# HTML
+
+		return anchor_tag
 	end
 end
