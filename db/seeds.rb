e1ca6efe269244f3c0ae4fbf9c1e7f539e3e141c
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-04-23 13:22:55 -0400

    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,6 @@ user_names = Array.new(3,'asdfasdf')
 user_emails = Array.new(3,'test@test.test')
 user_passwords = Array.new(3,'password')
 
-
-docs = Document.new
-
-titles = Array.new(3,'asdf')
-bodies = Array.new(3, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex eacommodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eufugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia deserunt mollitanim id est laborum.')
-
-3.times do |i|
-
-  Document.create(title: i.to_s + titles[i], body: i.to_s + bodies[i])
-
-end
-
 3.times do |i|
 
   User.create(username: user_names[i] + i.to_s, email: i.to_s + user_emails[i],
@@ -31,15 +19,29 @@ end
 
 User.create(username: 'greg', email: 'greg@greg.com', password: 'password', password_confirmation: 'password')
 
-annotations = Array(3, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, unde, in deserunt at voluptate aperiam beatae facere optio vero consectetur voluptates ullam illum natus soluta cum corrupti iusto accusamus possimus!")
+me = User.find_by(username: 'greg')
+
+
+docs = Document.new
+
+titles = Array.new(3,'asdf')
+bodies = Array.new(3, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex eacommodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eufugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia deserunt mollitanim id est laborum.')
+
+3.times do |i|
+
+  Document.create(title: i.to_s + titles[i], body: i.to_s + bodies[i], user_id: me.id)
+
+end
+
+annotations = Array.new(3, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, unde, in deserunt at voluptate aperiam beatae facere optio vero consectetur voluptates ullam illum natus soluta cum corrupti iusto accusamus possimus!")
 start_locations = [1,20,30]
 end_locations = [10, 29, 40]
 titles = ['so so good', 'penisfish', 'genius!']
 
 
 doc = Document.all.first
-me = User.find_by(username: 'greg')
+
 
 3.times do |i|
-	Annotation.new(start_location: start_locations[i], end_location: end_locations[i], user_id: me.id, document_id: doc.id, title: titles[i] )
+	Annotation.create(start_location: start_locations[i], end_location: end_locations[i], user_id: me.id, document_id: doc.id, title: titles[i], body: annotations[i] )
 end
