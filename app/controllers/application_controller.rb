420d369d3d0eac6b3559ecef6fc61cbb5efdf464
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-04-24 19:25:51 -0400

    moved them to application controller instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::Base
     current_user.reset_session_token!
     session[:session_token] = nil
   end
+
+  def require_current_user!
+    redirect_to new_user_url unless current_user
+  end
+
+  def require_no_current_user!
+    redirect_to root_url if current_user
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
-module ApplicationHelper
-
-  def require_current_user!
-    redirect_to new_user_url unless current_user
-  end
-
-  def require_no_current_user!
-    redirect_to root_url if current_user
-  end
-  
+module ApplicationHelper  
 end
