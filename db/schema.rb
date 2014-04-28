648b4fd65f2f05cbf129e75d9a828408e0b34c3c
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-04-28 14:13:51 -0400

    migrates multisearch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427021530) do
+ActiveRecord::Schema.define(version: 20140428181155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 20140427021530) do
     t.string   "notifiable_type"
     t.integer  "event_id"
     t.boolean  "is_read"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pg_search_documents", force: true do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
