e7ff995aa7935a0e81aa5212e082939122aec1e1
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-04-28 20:37:11 -0400

    multisearch.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,17 @@
 class PagesController < ApplicationController
 
   def homepage
-  	
+
   end
 
   def search
-  	if params[:query]
-  		@results = Document.search_documents(params[:query])
-  	else
-  		@results = PgSearch::Document
-  	end
-  	@results = @results.page(params[:page])
+    if params[:query]
+      @results = PgSearch.multisearch(params[:query])
+      # @results = PgSearch.search_documents(params[:query])
+    else
+      @results = PgSearch::Document
+    end
+    @results = @results.includes(:searchable).page(params[:page])
   end
 
 
