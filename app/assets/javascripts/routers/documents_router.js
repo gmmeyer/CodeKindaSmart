091c2fbb9e823c4a56457c49e3d7b8c9b1504265
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-02 01:17:12 -0400

    Adds swap to documents router.

--- app/assets/javascripts/routers/documents_router.js
+++ app/assets/javascripts/routers/documents_router.js
@@ -1,3 +1,11 @@
 CodeKindaSmart.Routers.Documents = Backbone.Router.extend({
 
+
+
+
+	_swapView: function (newView) {
+	  this._currentView && this._currentView.remove();
+	  this._currentView = newView;
+	  this.$rootEl.html(newView.render().$el);
+	}
 });
