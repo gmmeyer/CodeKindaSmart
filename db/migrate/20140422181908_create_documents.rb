6c7a4f47738ebea30e08e83b66fbd1e7a014c459
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-05 03:59:44 -0400

    makes migrations better

--- db/migrate/20140422181908_create_documents.rb
+++ db/migrate/20140422181908_create_documents.rb
@@ -12,10 +12,7 @@ class CreateDocuments < ActiveRecord::Migration
       t.timestamps
     end
 
-
-    add_index :documents, :body
     add_index :documents, :title, unique: true
-
     add_index :documents, :user_id
     add_index :documents, :author_id
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502031019) do
+ActiveRecord::Schema.define(version: 20140430171406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
