47e6f6750f6327633d698a0b99df75dd7b848822
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-09 15:02:25 -0400

    Adds more styling and content to annotations

--- app/assets/javascripts/views/documents/show.js
+++ app/assets/javascripts/views/documents/show.js
@@ -12,7 +12,8 @@ CodeKindaSmart.Views.DocumentsShow = Backbone.View.extend({
     "click .edit-document" : "editDocument",
     "click .delete-document" : "deleteDocument",
     "mousedown .document-body" : "startSelect",
-    "mouseup .document-body" : "handleSelect"
+    "mouseup .document-body" : "handleSelect",
+    "click .newAnnotation" : "newAnnotation"
   },
 
 
@@ -47,9 +48,7 @@ CodeKindaSmart.Views.DocumentsShow = Backbone.View.extend({
           CodeKindaSmart.annotationsActive = true
         }
       );
-
     }
-
   },
 
   renderAnnotation: function () {
@@ -76,8 +75,6 @@ CodeKindaSmart.Views.DocumentsShow = Backbone.View.extend({
   },
 
   startSelect: function(event) {
-    console.log(event)
-    console.log(this)
     var that = this;
 
     that.initialX = event.pageX;
@@ -88,32 +85,102 @@ CodeKindaSmart.Views.DocumentsShow = Backbone.View.extend({
   handleSelect: function(event) {
 
     var that = this;
+
+    that.finalX = event.pageX
+    that.finalY = event.pageY
+
+    $('.activeAnnotations').addClass('isHidden')
+    $('.activeAnnotations').removeClass('activeAnnotations')
     var sel = window.getSelection();
-    console.log(sel); // Delete me and die. Your debugging will kill you.
-    console.log(sel.rangeCount())
+    // console.log(sel); // Delete me and die. Your debugging will kill you.
 
     if (sel.anchorNode && sel.anchorNode.constructor.name === "HTMLLabelElement") {
-      console.log('death');
       return;
     }
 
     if (!that.started) {
-      console.log('other death');
       return;
     }
 
     if (sel.type !== "Range") {
-      console.log('not a range');
       return;
     }
 
-    if(sel.rangeCount) {
+    var range = sel.getRangeAt(0);
 
+    var lowEnd = sel.focusOffset < sel.anchorOffset ? sel.focusOffset : sel.anchorOffset;
+    var highEnd = sel.focusOffset < sel.anchorOffset ? sel.anchorOffset : sel.focusOffset;
+
+    var startIndex = this.selStartIndex(sel) + lowEnd;
+    var endIndex = startIndex + (highEnd - lowEnd);
+
+    that.start_location = CodeKindaSmart.doc.attributes.body.indexOf(range)
+    that.end_location = that.start_location + range.toString().length;
+    console.log(that.start_location)
+    console.log(that.end_location)
+
+    that.createButton(sel)
+  },
+
+
+  surroundRange: function (range) {
+      if (range) {
+          var el = document.createElement("span");
+          el.style.backgroundColor = "#9F3BFF";
+          el.style.color = "#fff"
+          try {
+              range.surroundContents(el);
+          } catch(ex) {
+          }
+      }
+  },
+
+  createButton: function (sel) {
+    var that = this;
+    this.annotationOffset = (that.finalY/2 + that.initialX/ 2) - 100 - $('.annotation-column').offset().top
+
+    console.log('boo')
+
+    if($('.newAnnotation')[0] != undefined) {
+      $(".newAnnotation").css("top", "=" + that.annotationOffset);
+      $(".newAnnotation").css("top", "+=" + that.annotationOffset);
+    } else {
+      content = $("<div class='group newAnnotation'> <a href=''> + </a> </div>")
+      console.log(content.html())
+      $(".annotation-column").append(content);
+      $(".newAnnotation").css("top", "+=" + that.annotationOffset);
+    }
+  },
+
+  newAnnotation: function(sel) {
+    event.preventDefault();
+  },
+
+  selStartIndex: function(sel) {
+    var node = sel.anchorNode.previousSibling;
+    var annRange = "";
+
+
+    while (node) {
+
+      if (node.nodeType === 3) {
+        annRange += node.data;
+      } else {
+        var $node = $(node).clone();
+        var $div = $("<div></div>");
+        $div.append($node);
+        annRange += $div.html();
+      }
+
+
+
+      node = node.previousSibling;
+      if (node === undefined) {
+        break;
+      }
     }
 
-    console.log(rangy.getSelection().getRangeAt(0))
-
-  } ,
-  
+    return annRange.length
+  }  
 
 });
