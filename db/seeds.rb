1d5455de350b7badd077c7a0a0cc6ec17c012eac
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-13 07:56:04 -0400

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,33 +40,6 @@ author_location = Array.new(3, "New York")
 	Author.create(name: author_names[i], location: author_location[i])
 end
 
-
-# docs = Document.new
-
-# titles = Array.new(3,'asdf')
-# bodies = Array.new(3, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex eacommodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eufugiat nulla pariatur. Excepteur sint occaecat cupidatatnon proident, sunt in culpa qui officia deserunt mollitanim id est laborum.')
-
-# bodies.length.times do |i|
-
-  # Document.create(title: i.to_s + titles[i], body: i.to_s + bodies[i], user_id: me.id, author_id: Author.first.id)
-
-# end
-
-# annotations = Array.new(4, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, unde, in deserunt at voluptate aperiam beatae facere optio vero consectetur voluptates ullam illum natus soluta cum corrupti iusto accusamus possimus!")
-# start_locations = [1, 20, 25, 30]
-# end_locations = [10, 29, 35, 40]
-# titles = ['so so good', 'gergz', 'genius!', 'another one']
-
-
-# doc = Document.all.first
-
-
-# annotations.length.times do |i|
-# 	Annotation.create(start_location: start_locations[i], end_location: end_locations[i], user_id: me.id, document_id: doc.id, title: titles[i], body: annotations[i])
-# end
-
-
-
 load('./seeds/cross_of_gold.rb')
-# load('./seeds/ulysses.rb')
+load('./seeds/ulysses.rb')
 load('./seeds/gpl.rb')
