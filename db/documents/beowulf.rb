7516bd8e75428d3d8795dbd9cd550d40740dcef1
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-13 09:16:17 -0400

    finishes beowulf seed

--- db/documents/beowulf.rb
+++ db/documents/beowulf.rb
@@ -4,8 +4,9 @@ def beowulf
 
 	my_annotation = Annotation.create(title: "I love this poem", body: "Seriously, this is my favorite of all the great epic poems.", start_location: 0, end_location: 200, user_id: @greg.id, document_id: beowulf.id)
 
-	@fake_users.each do |fake_user|
-		UpVote.create(annotation_id: my_annotation.id, user_id: fake_user.id)
+	User.all.each do |user|
+		next if user == @greg
+		UpVote.create(annotation_id: my_annotation.id, user_id: user.id)
 	end
 
 end
