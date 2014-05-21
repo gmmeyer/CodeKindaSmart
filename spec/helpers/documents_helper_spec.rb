94d57d33017ee5b43e4e126ad43274f456e512ef
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-20 22:01:17 -0400

    adds a test

--- spec/helpers/documents_helper_spec.rb
+++ spec/helpers/documents_helper_spec.rb
@@ -5,15 +5,19 @@ describe DocumentsHelper do
   describe "putting together a Document" do
 
     it "puts together a document properly" do
+
     end
 
     it "doesn't repeat words in the document" do
+        
     end
 
     it "correctly overlaps links" do
+
     end
 
     it "properly returns a document without annotations" do
+
     end
   end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -8,6 +8,7 @@ describe Document do
 			Document.find d.id
 		end
 		it 'will not save a document wthout a unique title' do
+			d = Document.create(title: 'Lorem', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nostrum quam quod at eaque rem mollitia provident maxime culpa exercitationem debitis molestiae ad nisi praesentium autem nobis sequi dolorum natus?', user_id: 3)
 			dd = Document.new(title: 'Lorem', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, itaque, nam, corporis excepturi quis distinctio officiis aut nesciunt placeat quo qui quibusdam labore recusandae nihil sapiente debitis eos vero. Ablig.", user_id: 4)
 			puts dd.valid?
 			dd.valid?.should be_false 
