414309159dbc67dccf407cda8f1d1f63b95dd937
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-23 15:34:46 -0400

    better annotations for ulysses

--- db/documents/ulysses.rb
+++ db/documents/ulysses.rb
@@ -6,6 +6,8 @@ def ulysses(me)
 
 	ulysses = Document.create(title: "Ulysses", body: ulysses_body, author_id: jj_author.id, user_id: jj.id)
 
-	Annotation.create(title: "my god...", body: "why did I upload this book? I've never even read it.", start_location: 0, end_location: ulysses_body.length - 1, document_id: ulysses.id, user_id: me.id)
+	Annotation.create(title: "So, that's why", body: "Even though I never read the book, I used it here as a showcase.", start_location: 60, end_location: 110, document_id: ulysses.id, user_id: me.id)
+
+	Annotation.create(title: "Why I uploaded this.", body: "This book is gigantic, I uploaded this to show that my database could handle a book that is, according to Wikipedia, \"265,000 words in length,\", and that \"uses a lexicon of 30,030 words\" It shows the flexibility of the implementation.", start_location: 0, end_location: 50, document_id: ulysses.id, user_id: me.id)
 
 end
