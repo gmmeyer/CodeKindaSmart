a432c2999c7612f41c8cdc081944a77dff8939d5
Greg Meyer <gregory.matthew.meyer@gmail.com>
2014-05-30 17:09:19 -0400

    small edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   before_filter :require_no_current_user!, only: [:new, :create]
 
   def new
-    @user = User.new()
+    @user = User.new
   end
 
   def create
